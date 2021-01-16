c1844f6646c2c8429b1333b84bb3df97745147f2
Ripytide <james.fortsterer@gmail.com>
2021-01-16 21:02:07 +0000

    Added newlines inbetween new password feedback instead of spaces using implode()

--- Back-End/Checklist/Settings/setPassword.php
+++ Back-End/Checklist/Settings/setPassword.php
@@ -31,32 +31,32 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 }
 
 function IsValidPassword($password){
-    $passErrorMsg = "";
+    $errorList = Array();
 
     //becomes true if there is no number in the password
     if (!preg_match("/.*[0-9].*/", $password)){
-        $passErrorMsg .= " Your password needs a number";
+        array_push($errorList, "Your password needs a number");
     }
 
     //becomes true if there is no lowercase letter in the password
     if (!preg_match("/.*[a-z].*/", $password)){
-        $passErrorMsg .= " Your password needs a lowercase letter";
+        array_push($errorList, "Your password needs a lowercase letter");
     }
 
     //becomes true if there is no uppecase letter in the password
     if (!preg_match("/.*[A-Z].*/", $password)){
-        $passErrorMsg .= " Your password needs an UPPERCASE letter";
+        array_push($errorList, "Your password needs an UPPERCASE letter");
     }
 
     if (mb_strlen($password) > 16){
-        $passErrorMsg .= " Your password is too long";
+        array_push($errorList, "Your password is too long");
     }
 
     if (mb_strlen($password) < 3){
-        $passErrorMsg .= " Your password is too short";
+        array_push($errorList, "Your password is too short");
     }
     
-    return($passErrorMsg);
+    return(implode("<br>", $errorList));
 }
 
 ?>
