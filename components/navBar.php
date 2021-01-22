61235482d743b6008f1bf07565d7932e8c52ab56
Ripytide <james.fortsterer@gmail.com>
2021-01-22 16:59:35 +0000

    fixed path to favicon for navbar as it was relative not absolute

--- components/navBar.php
+++ components/navBar.php
@@ -3,7 +3,7 @@ session_start();
 $loggedIn = isset($_SESSION["loggedin"]) && ($_SESSION["loggedin"] === true);
 ?>
 <header>
-	<div class="home"><img class="logo" src="favicon.ico" href="/index"><a href="/index">Checky</></div>
+	<div class="home"><img class="logo" src="/favicon.ico" href="/index"><a href="/index">Checky</></div>
 
 	<ul class="normNav">
 		<?php require("getNavList.php"); ?>
