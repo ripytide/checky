b5d91408b22ee1106206fec21c2deae942d706a8
ripytide <62516857+ripytide@users.noreply.github.com>
2021-03-30 15:18:55 +0100

    add comments

--- createChecklist.php
+++ createChecklist.php
@@ -1,8 +1,11 @@
 <?php
+//enable sql access functions such as query
 require($_SERVER['DOCUMENT_ROOT'] . "/Back-End/functions.php");
 
+//start session
 session_start();
 
+//define so useful boolean variable for use during logic section
 if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     $loggedin = true;
     $username = $_SESSION["username"];
@@ -10,22 +13,30 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     $loggedin = false;
 }
 
+//get a new ID that does not yet exist is the database
 $idArray = GetUniqueChecklistID(4);
 
+//if ID is in fact unique and the user is logged in
 if ($idArray["status"] === "unique" and $loggedin){
 
-    //open a new file which also creates it
+    //open a new file which also creates it which is what i want to do
     $newFile = fopen("checklists/" . $idArray["ID"] . ".php", "w");
 
     //make the new file require the generic checklist html
     fwrite($newFile, "<?php require(\"../checklist.php\"); ?>");
+    //close file to write changes
     fclose($newFile);
 
+    //add new checklist to the checklists table as private as is a logged in users'
     Query('INSERT INTO checklists VALUES (?, ?, NULL, NULL, "Private")', "ss", $idArray["ID"], $username);
 
+    //set status to success to tell the requestee that the operation was completed
     $output["status"] = "success";
+    
+    //send back the ID
     $output["checklistID"] = $idArray["ID"];
     
+//if the user is not logged in
 } else if ($idArray["status"] === "unique" and !$loggedin){
     
     //open a new file which also creates it
@@ -33,18 +44,27 @@ if ($idArray["status"] === "unique" and $loggedin){
 
     //make the new file require the generic checklist
     fwrite($newFile, "<?php require(\"../checklist.php\"); ?>");
+    
+    //close file to save changes
     fclose($newFile);
 
+    //add new checklist as public access mode
     Query('INSERT INTO checklists VALUES (?, NULL, NULL, NULL, "Public editable")', "s", $idArray["ID"]);
     
+    //send back a success status
     $output["status"] = "success";
+    
+    //send back the ID
     $output["checklistID"] = $idArray["ID"];
 } else {
+    //unexpected file code flow so send back fail status
     $output["status"] = "fail";
 }
 
+//redirect browser to the new checklist page
 header("Location: checklists/" . $idArray["ID"]);
 
+//a function to find a unique length string of length $length
 function GetUniqueChecklistID($length){
 
     $loop = true;
